35045ba9cc920f7a22fd7523659b1fe84f73885f
Pankaj-026 <pankajgupta1063@gmail.com>
2024-11-01 00:17:05 +0530

    Shop Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,10 +71,10 @@ addEventListener("scroll", shadowHeader);
 
 /* =============================== SCROLL UP =============================== */
 const scrollUp = () => {
-    const scrollUp = document.getElementById("scroll-up");
+  const scrollUp = document.getElementById("scroll-up");
 
-    this.scrollY >= 350
+  this.scrollY >= 350
     ? scrollUp.classList.add("show-scroll")
-    : scrollUp.classList.remove("show-scroll")
-  };
-  addEventListener("scroll", scrollUp);
+    : scrollUp.classList.remove("show-scroll");
+};
+addEventListener("scroll", scrollUp);
