5ede561ad93d3909ca3be00144871b120a98cebf
Pankaj-026 <pankajgupta1063@gmail.com>
2024-11-07 23:56:34 +0530

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,3 @@
-// /* =============================== SIGNIN =============================== */
-// const signinButton = document.getElementById("signin-button"),
-//     signinClose = document.getElementById("signin-close"),
-//     signinContent = document.getElementById("signin-content"),
-//     signupOnclick = document.getElementById('signup-onclick')
-
-// if(signinButton){
-//     signinButton.addEventListener( "click", () => {
-//         signinContent.classList.add("show-signin")
-//     })
-// }
-
-// if(signinClose){
-//     signinClose.addEventListener( "click", () => {
-//         signinContent.classList.remove("show-signin")
-//     })
-// }
-
-// /* =============================== SIGNUP =============================== */
-// const signupContent = document.getElementById("signup-content"),
-//     signinOnclick = document.getElementById('signin-onclick'),
-//     signupClose = document.getElementById("signup-close")
-
-// if(signupClose){
-//     signupClose.addEventListener( "click", () => {
-//         signupContent.classList.remove("show-signup")
-//     })
-// } 
-
-// /* =============================== ONCLICK =============================== */
-// if(signupOnclick){
-//     signupOnclick.addEventListener("click", () => {
-//         signinContent.classList.remove("show-signin")
-//         signupContent.classList.add("show-signup")
-//     })
-// }
-
-// if(signinOnclick){
-//     signinOnclick.addEventListener("click", () => {
-//         signupContent.classList.remove("show-signup")
-//         signinContent.classList.add("show-signin")
-//     })
-// }
-
 /* =============================== TOGGLE BUTTON =============================== */
 const toggleButton = document.getElementById("nav-toggle"),
   toggleContent = document.getElementById("nav-menu"),
@@ -60,21 +16,20 @@ if (toggleClose) {
 }
 
 /* =============================== HEADER SHADOW =============================== */
-const shadowHeader = () => {
+addEventListener("scroll", () => {
   const header = document.getElementById("header");
 
   this.scrollY >= 30
     ? header.classList.add("shadow-header")
     : header.classList.remove("shadow-header");
-};
-addEventListener("scroll", shadowHeader);
+});
 
 /* =============================== SCROLL UP =============================== */
-const scrollUp = () => {
+
+addEventListener("scroll", () => {
   const scrollUp = document.getElementById("scroll-up");
 
   this.scrollY >= 350
     ? scrollUp.classList.add("show-scroll")
     : scrollUp.classList.remove("show-scroll");
-};
-addEventListener("scroll", scrollUp);
+});
